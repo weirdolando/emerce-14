1744e05a0a67039eefb1a54c20ed55561a0523da
weirdolando <93187436+weirdolando@users.noreply.github.com>
2023-04-13 21:05:09 +0700

    init

---
+++ server/index.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const db = require("./config/db");
+const PORT = process.env.PORT;
+db.connect((err) => {
+  if (err) return console.error(err);
+  console.log("Connected to MySQL");
+});
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
