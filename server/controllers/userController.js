8f831377dfca927fc5f19d6a0bb2ccdfd9c21cfa
weirdolando <93187436+weirdolando@users.noreply.github.com>
2023-04-14 20:41:27 +0700

    Add auth feature (server)

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,36 @@
 const db = require("../config/db");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+async function auth(req, res) {
+  const { email, password } = req.body;
+  if (!email || !password)
+    return res.status(400).json({ error: "Required field cannot be empty" });
+  try {
+    const [user] = await db
+      .promise()
+      .query(
+        "SELECT id, username, email, password FROM users WHERE email = ?",
+        [email]
+      );
+    if (!user.length)
+      return res.status(401).json({ error: "Email is not registered" });
+    const isValid = bcrypt.compareSync(password, user[0].password);
+    if (!isValid) return res.status(401).json({ error: "Password incorrect" });
+    // !FIXME: Change expires to a longer time in production
+    const token = jwt.sign({ id: user[0].id }, process.env.SECRET, {
+      expiresIn: "5m",
+    });
+    return res.status(200).json({
+      message: "Login success",
+      data: { username: user[0].username, email: user[0].email },
+      token,
+    });
+  } catch (err) {
+    console.log(err.sqlMessage);
+    return res.status(400).json({ error: err.sqlMessage });
+  }
+}
 
 async function register(req, res) {
   const { email, password, phone, firstName, lastName, username } = req.body;
@@ -20,7 +51,7 @@ async function register(req, res) {
         email,
         hashPassword,
       ]);
-    const [user, userFields] = await db
+    const [user] = await db
       .promise()
       .query("SELECT id FROM users WHERE username = ? AND email = ?", [
         username,
@@ -33,7 +64,7 @@ async function register(req, res) {
         "INSERT INTO user_profiles (user_id, firstName, lastName) VALUES(?, ?, ?)",
         [userId, firstName, lastName]
       );
-    const [userProfile, userProfileFields] = await db
+    const [userProfile] = await db
       .promise()
       .query("SELECT id FROM user_profiles WHERE user_id = ?", [userId]);
     const userProfileId = userProfile[0].id;
@@ -61,4 +92,4 @@ async function register(req, res) {
   }
 }
 
-module.exports = { register };
+module.exports = { auth, register };
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,6 +1,7 @@
 const userRouter = require("express").Router();
 const { userController } = require("../controllers");
 
+userRouter.post("/auth", userController.auth);
 userRouter.post("/register", userController.register);
 
 module.exports = userRouter;
