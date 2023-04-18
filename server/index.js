94c8840d2cea16e651336955cd05c400e4437cda
Anak Agung Gde Bagus Janapriya <48237083+janapr7@users.noreply.github.com>
2023-04-18 13:52:46 +0800

    create back end product (#1)

--- server/index.js
+++ server/index.js
@@ -4,6 +4,9 @@ const cors = require("cors");
 
 const db = require("./config/db");
 const PORT = process.env.PORT;
+
+const { productRouter } = require("./routers")
+
 db.connect((err) => {
   if (err) return console.error(err);
   console.log("Connected to MySQL");
@@ -12,6 +15,7 @@ db.connect((err) => {
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use("/product", productRouter)
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
